7945c95281ff84328c15d6aa75cf720e62076a0f
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-07-03 16:53:03 +0300

    Added composition

--- src/main/java/com/vztot/impl/ExecutorServiceCalculation.java
+++ src/main/java/com/vztot/impl/ExecutorServiceCalculation.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class ExecutorServiceCalculation {
-
     private ExecutorService executorService;
     private List<CallableClass> listOfCallables;
 
